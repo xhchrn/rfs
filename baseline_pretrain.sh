3c6e4370f6719e888a334b07e6764912e70c08b8
Xiaohan Chen <chenrxh@tamu.u>
2020-05-24 14:53:16 +0200

    scripts

---
+++ baseline_pretrain.sh
@@ -0,0 +1,19 @@
+#======================
+# exampler commands on miniImageNet
+# ======================
+
+# supervised pre-training
+python train_supervised.py --trial pretrain \
+	--model_path ./checkpoints --tb_path ./tb \
+	--dataset CIFAR-FS --data_root data/CIFAR-FS \
+	--epochs 90 --lr_decay_epochs 45,60,75
+
+# distillation
+# python train_distillation.py -r 0.5 -a 1.0 \
+# 	--path_t checkpoints/resnet12_CIFAR-FS_lr_{}-pretrain/resnet12_last.pth \
+# 	--trial born1 --model_path ./checkpoints --tb_path ./tb \
+# 	--dataset CIFAR-FS --data_root data/CIFAR-FS \
+# 	--epochs 90 --lr_decay_epochs 45,60,75
+# 
+# # evaluation
+# python eval_fewshot.py --model_path /path/to/student.pth --data_root /path/to/data_root
